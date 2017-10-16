00558eafbc2fcaff91f1e49a9e9e850064aa0a4d
Lydia <leedush21@gmail.com>
2017-10-16 21:53:13 +0300

    inital setup

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import{BookmarkComponent} from './bookmark.component'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>{{name}}</h1>`
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
+  name='My First Bookmark';
 }
