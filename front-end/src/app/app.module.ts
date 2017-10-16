551e3744a2b27269dec6bbcf770f913ae997cbd4
Lydia <leedush21@gmail.com>
2017-10-16 21:52:50 +0300

    import and declare bookmark component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,11 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BookmarkComponent } from './bookmark.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent, BookmarkComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
